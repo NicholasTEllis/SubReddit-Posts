29ae88bb8d8f9238fe0552083f3d80f42900f89e
NicholasTEllis <e.nicholas72@yahoo.com>
2017-01-30 14:13:19 -0700

    all done with it

---
+++ NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  SubReddit Posts
+//
+//  Created by Nicholas Ellis on 1/30/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+import Foundation
